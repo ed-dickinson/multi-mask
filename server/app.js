4ea9a84b6b6890df7cc0dea6f5e6892d27be61cd
ed-dickinson <edwardejd@gmail.com>
2021-09-06 18:43:15 +0100

    prod adds

--- server/app.js
+++ server/app.js
@@ -4,11 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var compression = require('compression');
+var helmet = require('helmet');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var cors = require('cors');
 var app = express();
+
+app.use(helmet());
 
 require('dotenv').config()
 const DBkey = process.env.DB_URL;
@@ -24,6 +29,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,6 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
